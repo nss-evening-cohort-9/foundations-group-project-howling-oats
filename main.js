9aabc3397d462143fb46d18947e678bb4c58afb2
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-03-30 11:53:52 -0500

    WIP

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-console.log('project');
+const events = [
+{
+name: 'Clockwork Tasting',
+description: 'We will serve you disturbingly tiny glasses of beer, but many of them, in the surprisingly capacious workings of our grandfather clock. The door is in the back.',
+image: 'https://www.theclockdepot.com/images/D/Corner-Grandfather-Clock-611-252-HR.jpg',
+imageAlt: 'Image of our grandfather clock, which put in an appearance this afternoon',
+date: '04/26/2019',
+address: '123 The Other Way, Nook Grandfather Clock, Nashville, 37211',
+attendees: ['Mary', 'Daniel', 'Mark', 'James Hook']
+},
+{
+name: 'The Haunted Bottles One-Night Smash',
+description: 'Join us for a night of ghostly music. If you\'ve never heard a ghost sing from a bottle, this event is for you. Also door prizes!',
+image: './images/bottle-picture.png',
+imageAlt: 'Image of several bottles, each a different color and haunted by a very nice spirit',
+date: '10/31/2019',
+address: '123 The Other Way, Ghost-hosting Room, Nashville, 37211',
+attendees['Mary', 'Daniel', 'Mark', 'Ebeneezer']
+},
+{
+    name: 'Echo\'s SpokenWord',
+    description: 'An "open mirror" night, in which you speak heartfelt poetry to our mirror and your words are echoed back to you. Your reflection is definitely not alive.',
+    image: 'https://www.t2day.com/ebay/Vanity_Makeup_Mirror/Hand_Held_Mirror/Vintage_Antique_Style_Girls_Princess_Oval_Roune_Vanity_Makeup_Hand_Held_Mirror_Silver_Small_01.jpg',
+    imageAlt: 'Image of our handheld mirror, with silver embroidery around the edge. Not magical in the least.',
+    date: '05/01/2019',
+    address: '123 The Other Way, Room Of Disappearance, Nashville, 37211',
+    attendees['Mary', 'Daniel', 'Mark', 'Anastasia']
+    },{
+        name: 'Nashville Bar Festival',
+        image: ,
+        imageAlt: ,
+        date: ,
+        address: ,
+        attendees[]
+        }
+];
