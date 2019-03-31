ce62835035e513bdafcc909b77a088bdb8310501
Mark Okafor <okaformark@gmail.com>
2019-03-31 17:33:19 -0500

    wip

--- main.js
+++ main.js
@@ -109,6 +109,43 @@ const buyBeerCardBuilder = () => {
 }
 // end of beer card builder
 
+const addClickEvents = (e) => {
+    const nav = e.target.id;
+    if (nav === 'navToHome') {
+        document.getElementById('navToHome').addEventListener('click', function (e) {
+        e.preventDefault()
+    // daniel's ||| document.getElementById('beer-page').classList.add('hideStuff')
+        document.getElementById('eventsPage').classList.add('hideStuff');
+        document.getElementById('beer-page').classList.add('hideStuff');
+        document.getElementById('home-page-container').classList.remove('hideStuff');
+    })
+}
+    // waiting for daniel's page
+    // document.getElementById('navToHome').addEventListener('click', function (e) {
+    //     e.preventDefault()
+    //      document.getElementById('beer-page').classList.add('hideStuff')
+    //      document.getElementById('eventsPage').classList.add('hideStuff')
+    //      document.getElementById('beer-page').classList.add('hideStuff')
+    //      document.getElementById('home-page-container').classList.remove('hideStuff')
+    //  })
+    // document.getElementById('navToEvents').addEventListener('click', function (e) {
+    //     e.preventDefault()
+    //     document.getElementById('home-page-container').classList.add('hideStuff')
+    //      //document.getElementById('beer-page').classList.add('hideStuff')
+    //     document.getElementById('eventsPage').classList.remove('hideStuff')
+    //     document.getElementById('beer-page').classList.add('hideStuff') 
+    //  })
+    else if (nav === 'navTobuy'){
+     document.getElementById('navToBuy').addEventListener('click', function (e) {
+        e.preventDefault();
+        document.getElementById('home-page-container').classList.add('hideStuff')
+         //document.getElementById('beer-page').classList.add('hideStuff')
+        document.getElementById('eventsPage').classList.add('hideStuff')
+        document.getElementById('beer-page').classList.remove('hideStuff') 
+     })
+    }
+   };
+
 const clearFormInput = () => {
     document.getElementById('nameInput').value = '';
     document.getElementById('emailInput').value = '';
@@ -122,5 +159,6 @@ const submitEvent = () => {
 const init = () => {
 submitEvent();
 buyBeerCardBuilder();
+addClickEvents();
 };
 init();
