144b8529efd2baad843c09b575ae5e678c01433f
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-03-31 16:17:54 -0500

    WIP

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const events = [{
         name: 'Clockwork Tasting',
         description: 'We will serve you disturbingly tiny glasses of beer, but many of them, in the surprisingly capacious workings of our grandfather clock. The door is in the back.',
         image: 'https://www.theclockdepot.com/images/D/Corner-Grandfather-Clock-611-252-HR.jpg',
-        imageAlt: 'Image of our grandfather clock, which put in an appearance this afternoon',
+        imageAlt: 'our grandfather clock, which put in an appearance this afternoon',
         date: '04/26/2019',
         address: '123 The Other Way, Nook Grandfather Clock, Nashville, 37211',
         attendees: ['Mary', 'Daniel', 'Mark', 'James Hook']
