f9e33dc0dc8c34955d4ff511790206b90752df38
Mary West <westmarye1219@gmail.com>
2019-04-01 20:30:08 -0500

    merge conflicts resolved

--- main.js
+++ main.js
@@ -69,45 +69,7 @@ const brewmasters = [
                 attendees: ['Mary', 'Daniel', 'Mark', 'Sarah']
                 }
         ];
-        // beer data
-        const beers = [
-            {
-                title: 'Vintage T Shirt',
-                imageUrl:'https://img.etsystatic.com/il/380a33/1548068326/il_570xN.1548068326_km2w.jpg?version=0',
-                description: 'Shirt',
-                price: '$20.00',
-                },
-                {
-                title: 'Exclusive Variety Six Pack',
-                imageUrl: 'https://www.wegmans.com/content/dam/wegmans/products/560/20560.jpg',
-                description: 'Six Pack',
-                price: '$10.00',
-                },
-                {
-                title: 'Beer Hat',
-                imageUrl:'https://images-na.ssl-images-amazon.com/images/I/71PjjenZxdL._SL1500_.jpg',
-                description: 'picture of item',
-                price: '$15.00',
-                },
-                {
-                title: 'Bar Key',
-                imageUrl:'https://i.etsystatic.com/13657333/d/il/b5dcbd/1202095436/il_340x270.1202095436_8me5.jpg?version=0',
-                description: 'Bar Key',
-                price: '$20.00',
-                },
-                {
-                title: 'Pint Glass',
-                imageUrl:'https://images.crateandbarrel.com/is/image/Crate/PintTumblerWCrownSHS16/?$web_product_hero$&160203172057&wid=625&hei=625',
-                description: 'Glass',
-                price: '$12.00',
-                },
-                {
-                title: 'Coffee Mug',
-                imageUrl:'https://i.pinimg.com/236x/64/de/7a/64de7abcd9b031bd3eac82badde2a038--the-coffee-coffee-mugs.jpg',
-                description: 'Mug',
-                price: '$8.00',
-            },
-        ]
+
 const brewmastersBuilder = (brewmasters) => {
     let domString = '';
     brewmasters.forEach((brewmaster) =>{
@@ -342,7 +304,6 @@ eventsCardBuilder();
 buyBeerCardBuilder();
 submitEvent();
 setupListeners();
-addEvents();
 brewmastersBuilder(brewmasters);
 
 };
